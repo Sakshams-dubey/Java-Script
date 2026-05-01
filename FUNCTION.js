60dead31c3ccefc92a87f4c037602fee28f7d054
saksham dubey <sakshamd1305@gmail.com>
2026-05-01 11:50:49 +0530

    Java script Function

---
+++ FUNCTION.js
@@ -0,0 +1,134 @@
+// function declaration
+// function defination
+// function calling
+
+// syntax
+
+// function identifire()      ---- declaration
+// {          
+//     code                   ---- defination
+// }
+                         //   ---- calling                                                   
+
+
+// function demo()
+// {
+//     let a=parseInt(prompt("enter number"))
+//     let b=parseInt(prompt("enter number"))
+//     let c=a+b
+//     console.log(c);
+    
+// }
+// demo()
+
+
+
+// argument and parameter 
+
+// function fun1(PARAMETER){
+
+// }
+// fun1(value)
+
+
+// function demo(a){
+//     return a+a
+// }
+// console.log(demo(8));  //return value
+
+
+// with argument and no return value
+
+// function demo(X)
+// {
+//     console.log(X+X);
+    
+// }
+//     let a=parseInt(prompt("enter number"))
+
+// demo(a)
+
+
+// WAP to print a table of a number using function type two with argument and no return.
+
+
+// function demo(num)
+// {
+//     for(let a=1 ; a<=10 ; a++)
+//     {
+//     console.log(num*a);
+
+//     }
+    
+// }
+//     let num=parseInt(prompt("enter number"))
+
+// demo(num)
+
+
+// without argument and return value
+
+// function demo1(){
+//     return "javascript"
+// }
+// console.log(demo1());
+
+
+// with argument and return value
+
+// function demo2 (a,b){
+//     return a+b
+// }
+
+// let a=parseInt(prompt("enter number"))
+// let b=parseInt(prompt("enter number"))
+// console.log(demo2(a,b));
+
+
+
+// named function
+// anonymus function
+
+// ARROW FUNCTION
+
+// basic syntax of arrow function
+
+// ()=>{return}
+
+// ()=>()
+
+// ()=>
+
+// =>
+
+// =>()
+
+
+// let fun = ()=>{return "code"}
+
+// console.log(fun());
+
+
+// let sum = (a,b)=>{return a+b}
+
+// console.log(sum(12,3));
+
+
+
+// let num = (a)=>(a+a)
+// let ans = num(20)
+// console.log(ans);
+
+
+
+// let num = ()=>(2+2)
+// let ans = num()
+// console.log(ans);
+
+
+// let demo = x => x+x
+
+// console.log(demo(3));
+
+
+
