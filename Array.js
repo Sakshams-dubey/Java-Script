6dacf2d0bcb7e2da7d22418b121f5e991f69113d
Sakshams-dubey <sakshamd1305@gmail.com>
2026-05-06 13:28:53 +0530

    Array

---
+++ Array.js
@@ -0,0 +1,91 @@
+      //  ARRAY
+
+
+// let stu=["saksham",19,6.1,"bhopal"]
+// console.log(stu)
+
+// for(let a=0 ; a<=4 ; a++){
+//     console.log(stu[a])
+// }
+
+
+
+// let p=[]
+
+// for(let a=0 ; a<=4 ; a++){
+//     p[a]=prompt("enter element :- ")
+// }
+
+// console.log(p)
+
+
+// WAP to print a sum of all elements between  8-1.
+
+// let p=[8,7,6,5,4,3,2,1]
+// let sum=0
+// for(let a=0 ; a<(p.length) ; a++){
+//     sum = sum + p[a]
+// }
+//     console.log(sum)
+
+
+// ... :- spread operator or rest operator
+
+// let ar1 = ["saksham","ali","pradosh","jay bhai","avibh"]
+// let ar2 = ["bhupendra","chandan","tanishk"]
+
+// let ar3 = [...ar1,...ar2]
+// console.log(ar3)
+
+
+// Destructuring of array
+
+// let [name,age,city,height] = ["rishabh",23,"bhopal",5.10]
+// console.log(age);
+ 
+// let [name,age, ,height] = ["rishabh",23,"bhopal",5.10]
+// console.log(height);
+
+
+// for of loop
+
+// let emp = ["deepali", "bhopal",12345678910,"cybrom"]
+
+// for(let v of emp){
+//     console.log(v);
+// }
+
+
+// print square of all even numbers between 1-10
+
+// let num = [1,2,3,4,5,6,7,8,9,10]
+
+// for(let v of num){
+//     if(v%2==0){
+//     console.log(v);
+// }
+// }
+
+
+// let stu = ["aniruddh","unmarrid",18,"65kg","indore"]
+
+// console.log(stu);
+
+// let arry = stu.slice(1,4)
+// console.log(arry)
+
+
+// let stu = ["aniruddh","unmarrid",18,"65kg","indore"]
+
+// stu.splice(index no, no of elements, elements)
+
+// stu.splice(2,3)
+// console.log(stu)
+
+
+// let stu = ["aniruddh","unmarrid",18,"65kg","indore"]
+
+// stu.splice(index no, no of elements, elements)
+
+// stu.splice(2,0,"orange")
+// console.log(stu)
