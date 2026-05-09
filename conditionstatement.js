c847b4377e13cb6420bc309626defb3786527bad
Sakshams-dubey <sakshamd1305@gmail.com>
2026-05-09 19:02:32 +0530

    Conditional statement

---
+++ conditionstatement.js
@@ -0,0 +1,127 @@
+//let num = parseInt(prompt("Enter a number:- "))
+
+// if(num > 0){
+//     console.log("The number is positive")
+// }  
+// else{
+//     console.log("The number is negative")
+// }
+
+
+
+// let num = parseInt(prompt("Enter a number:- "))
+
+// if(num % 2!= 0){
+//     console.log("The number is odd",num*num)
+// }
+// else{
+//     console.log("The number is even",3.14*num*num)
+// }
+
+
+
+// let num = prompt("Enter a character:- ")
+
+// if(num == 'a'){
+//     console.log("The character is vowel")
+// }
+// else if(num == 'e'){
+//     console.log("The character is vowel")
+// }
+// else if(num == 'i'){
+//     console.log("The character is vowel")
+// }
+// else if(num == 'o'){
+//     console.log("The character is vowel")
+// }
+// else if(num == 'u'){
+//     console.log("The character is vowel")  
+// }
+// else{
+//     console.log("The character is constant")
+//}
+
+
+
+// Q1) WAP to take 1 int input from the user. you have to perform calculation based on input, when user give 1 int input so you have to perform addition of two Number, 2 for sub, 3 for multiplication, 4 for division. Otherwise print invalid, using ladder if else.
+
+// num = parseInt(prompt("enter number :- "))
+
+// if(num==1){
+//     a = parseInt(prompt("enter 1st number :- "))
+//     b = parseInt(prompt("enter 2nd number :- "))
+//     console.log("addition :- ",a+b)
+// }
+// else if(num==2){
+//     a = parseInt(prompt("enter 1st number :- "))
+//     b = parseInt(prompt("enter 2nd number :- "))
+//     console.log("addition :- ",a-b)
+// }
+// else if(num==3){
+//     a = parseInt(prompt("enter 1st number :- "))
+//     b = parseInt(prompt("enter 2nd number :- "))
+//     console.log("addition :- ",a*b)
+// }
+// else if(num==4){
+//     a = parseInt(prompt("enter 1st number :- "))
+//     b = parseInt(prompt("enter 2nd number :- "))
+//     console.log("addition :- ",a/b)
+// }
+// else{
+//     console.log("INVALID")
+// }
+
+
+
+// Q2) WAP to find grade of student according to percentage take 5 inputs (subject marks) then find percentage then print grade accordingly.
+
+// c++ = 99
+// js = 100
+// ui/ux = 98
+// html/css = 100
+// python = 99
+
+
+
+// let c = parseInt(prompt("enter c++ marks :- "))
+// let js = parseInt(prompt("enter js marks :- "))
+// let uiux = parseInt(prompt("enter uiux marks :- "))
+// let htmlcss = parseInt(prompt("enter htmlcss marks :- "))
+// let python = parseInt(prompt("enter python marks :- "))
+
+// let totalMarks = c+js+uiux+htmlcss+python
+// let percentage = totalMarks/5
+// console.log("totalMarks :- " + totalMarks)
+// console.log("percentage :- " + percentage+"%")
+
+// if(percentage>45 && percentage<=60){
+//     console.log("D Grade")
+// }
+// else if(percentage>60 && percentage<=75){
+//     console.log("C Grade")
+// }
+// else if(percentage>75 && percentage<=90){
+//     console.log("B Grade")
+// }
+
+// else if(percentage>90 && percentage<=100){
+//     console.log("A Grade")
+// }
+// else{
+//     console.log("FAIL")
+// }
+
+
+// Q3) WAP to take 3 int inputs from the user and check 1 input is divisible by 3 or Not.  if number is divisible by 3 then find area of rectangle of 1 and 2 input. if number is not divisible by 3 then add all input and print it.
+
+
+// let a = parseInt(prompt("enter a :- "))
+// let b = parseInt(prompt("enter b :- "))
+// let c = parseInt(prompt("enter c :- "))
+
+// if(a%3==0){
+//     console.log("area of rectangle :- " + a*b)
+// }
+// else{
+//     console.log(a+b+c)
+// }
