93ff44049b65b3b74e0ddcaaa2e00c91ed4aa971
Sakshams-dubey <sakshamd1305@gmail.com>
2026-05-14 14:17:18 +0530

    Try

---
+++ try.js
@@ -0,0 +1,65 @@
+// Q1. Write a JavaScript program to print 1 to 20 even number using while loop.
+
+// let a=1
+// while(a<=20){
+//     if(a%2==0){
+//         console.log(a);
+//     } 
+//     a++
+// }
+
+
+
+// Q2 . Write a Java script code to find factorial of a given number.	
+
+// let num = 10
+// let fact = 1
+// while(num>=1){
+//     fact = fact * num
+//     num--
+// }
+// console.log(fact);
+
+// Q3. Create an array object and Apply any six array methods in your program.   
+
+// let sub = ["python","c++","js","java"]
+// console.log(sub);
+
+// sub.splice(1,2,"uiux")
+// console.log(sub);
+
+// sub.pop()
+// console.log(sub);
+
+// sub.push("c")
+// console.log(sub);
+
+// sub.slice(1,3)
+// console.log(sub);
+
+// sub.unshift("uiux")
+// console.log(sub);
+
+// sub.shift("uiux")
+// console.log(sub);
+
+
+// function changeColor(){
+//     document.getElementById("change").style.backgroundColor="orange"
+// }
+
+
+// Q5. Write a Java Script Programm to create Counter Programm using setInterval() method.
+
+// let v=0;
+// let interval 
+// function start(){
+//     interval = setInterval(()=>{
+//         v++;
+//         document.getElementById("text").innerText = v;
+//     },1000)     
+// }
+
+// function stop(){
+//     clearInterval(interval)
+// }
